bc688f4108620d485e8d750cd4a7b65520f89184
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-15 19:23:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
       :twitter => doc.css("div.social-icon-container a")[0].attribute('href').value if doc.css("div.social-icon-container a")[0].attribute('href').value ,
       :linkedin => doc.css("div.social-icon-container a")[1].attribute('href').value if doc.css("div.social-icon-container a")[1].attribute('href').value, 
       :github => doc.css("div.social-icon-container a")[2].attribute('href').value if doc.css("div.social-icon-container a")[2].attribute('href').value, 
-      :blog => doc.css("div.social-icon-container a")[3].attribute('href').value,  
+      :blog => doc.css("div.social-icon-container a")[3].attribute('href').value if doc.css("div.social-icon-container a")[3].attribute('href').value,  
       :profile_quote => doc.css(".profile-quote").text, 
       :bio => doc.css("div.bio-content.content-holder div.description-holder p" ).text #a more specific selector was requiring nesting down css
     }
