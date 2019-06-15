987acdd51536b487f7f5181df97838c34ac2a30c
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-15 19:43:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
     doc = Nokogiri::HTML(open(profile_url))
   # binding.pry
     student = {}
-    links = doc.css("div.social-icon-container").children.css("a").collect { |i| i.attribute('href').value}
+    links = doc.css("div.social-icon-container a").collect { |i| i.attribute('href').value}
     links.each do |link|
       if link.include?("linkedin")
         student[:linkedin] = link
