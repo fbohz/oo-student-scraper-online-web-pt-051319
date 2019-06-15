7d99a3e64d601d02aa82c3b685a6c2833349441b
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-15 19:39:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
     doc = Nokogiri::HTML(open(profile_url))
   # binding.pry
     student = {}
-    case 
+    case doc
     when doc.css("div.social-icon-container a")[0].attribute('href').value
       student[:twitter] = doc.css("div.social-icon-container a")[0].attribute('href').value
     when doc.css("div.social-icon-container a")[1].attribute('href').value   
